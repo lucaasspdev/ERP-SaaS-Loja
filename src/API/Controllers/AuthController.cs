a90823c1ed9ffcc3194e89183e855764434daa43
Lucas Santos <lucas58227ssp@outlook.com>
2026-02-22 15:53:30 -0300

    connectionDBApp

--- src/API/Controllers/AuthController.cs
+++ src/API/Controllers/AuthController.cs
@@ -13,17 +13,17 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        public static User user = new();
+        public static User user = new() { Email = "", SenhaCriptografada = "" };
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserDTO request)
         {
             var senhaCriptografada = new PasswordHasher<User>()
-                .HashPassword(user, request.senha);
+                .HashPassword(user, request.Senha);
 
 
-            user.email = request.email;
-            user.senhaCriptografada = senhaCriptografada;
+            user.Email = request.Email;
+            user.SenhaCriptografada = senhaCriptografada;
 
             return Ok(user);
         }
@@ -32,9 +32,9 @@ namespace API.Controllers
         public ActionResult<string> Login(UserDTO request)
         {
             var result = new PasswordHasher<User>()
-                .VerifyHashedPassword(user, user.senhaCriptografada, request.senha);
+                .VerifyHashedPassword(user, user.SenhaCriptografada, request.Senha);
 
-            if (result == PasswordVerificationResult.Failed || request.email != user.email)
+            if (result == PasswordVerificationResult.Failed || request.Email != user.Email)
                 return BadRequest("Senha ou usuário incorretos");
 
             return Ok("Login bem-sucedido");
