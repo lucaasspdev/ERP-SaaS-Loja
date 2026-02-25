15deacda9184c0724096a4db147efd5b147875a9
Lucas Santos <lucas58227ssp@outlook.com>
2026-02-25 00:34:25 -0300

    ajuste de pasta

--- MeuERP/backend/src/API/obj/Debug/net9.0/ERPLoja.AssemblyInfo.cs
+++ MeuERP/backend/src/API/obj/Debug/net9.0/ERPLoja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPLoja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d090ef61b029f129d3c63dae3680c4b0ad1c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0981925cf74d92cb052923e90d32b7c865a1cd01")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPLoja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPLoja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
